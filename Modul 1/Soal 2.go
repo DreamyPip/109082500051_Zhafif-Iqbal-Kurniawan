598afe00154e06cfc8b97fb73fc23242007d7772
Zhafif Iqbal Kurniawan <zhafif.iqbal.123@gmail.com>
2026-03-11 00:41:21 +0700

    first commit

---
+++ Modul 1/Soal 2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		wrna1, wrna2, wrna3, wrna4 string
+		temp                       bool
+	)
+	fmt.Println("Urutkan wrna sesuai rules")
+	temp = true
+	x := 1
+	for x <= 5 {
+		fmt.Print("Percobaan ", x, " : ")
+		fmt.Scan(&wrna1, &wrna2, &wrna3, &wrna4)
+		if wrna1 != "merah" || wrna2 != "kuning" || wrna3 != "hijau" || wrna4 != "ungu" {
+			temp = false
+		}
+		x++
+	}
+	fmt.Println("Berhasil :", temp)
+}
